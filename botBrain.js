df3acc24cdc928de2b52f5b2d665fc0ca82ed191
Nasr-eddine Kargas <NasrKargas@gmail.com>
2015-07-29 23:48:52 +1000

    no message

--- botBrain.js
+++ botBrain.js
@@ -30,7 +30,7 @@ var BotBrain = (function(botName, slackSelf) {
 	        imageURL        = box.find('img').first().attr('src');
 	    }
 	    
-	    return "it is "fishName + " " + imageURL + " " + binomialName;
+	    return "it is " + fishName + " " + imageURL + " " + binomialName;
 	};
 
 	function callWikipediaAPI(wikipediaPage) {

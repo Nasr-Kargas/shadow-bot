d92abc1e0342ea96189a123f624f55f46027ee57
Nasr-eddine Kargas <NasrKargas@gmail.com>
2015-07-29 23:07:14 +1000

    Minor bug fix

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var express = require('express');
 var app = express();
 var botMaster = "@nasrkargas";
 var shouldTalk = true;
+var bot;
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
